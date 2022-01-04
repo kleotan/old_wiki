a59b80807b5721dd80a3c0bf56d34d79ef9bb520
kleotan <94611863+kleotan@users.noreply.github.com>
2022-01-04 14:27:52 +0200

    Delete encyclopedia directory

--- encyclopedia/forms.py
+++
@@ -1,9 +0,0 @@
-from typing import ContextManager
-from django import forms
-from django.forms.widgets import Textarea
-
-class NewEntryForm(forms.Form):
-    title=forms.CharField (label="Заголовок")
-    content=forms.CharField(widget=forms.Textarea(attrs={'cols':60, 'row': 20}), label="Нова стаття")
-
-
